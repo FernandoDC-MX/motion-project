15a6c6649037d1685ce7d9adb2c3629c9278ef14
FernandoDC-MX <fernando@dcircuits.mx>
2022-05-18 16:06:54 -0500

    Cambio en el bind

--- addon.cc
+++ addon.cc
@@ -1,21 +1,36 @@
 #include <nan.h>
 
-void Sum(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  v8::Isolate* isolate = args.GetIsolate();
+const int maxValue = 10;
+int numberOfCalls = 0;
 
-  int i;
-  double a = 3.1415926, b = 2.718;
-  for (i = 0; i < 10000000; i++){
-    a+= b;
+NAN_METHOD(WhoAmI) {
+  auto message = Nan::New<v8::String>("I'm a Node Hero!").ToLocalChecked();
+  info.GetReturnValue().Set(message);
+}
+
+NAN_METHOD(Increment) {
+  if (!info[0]->IsNumber()) {
+    Nan::ThrowError("Argument must be a number");
+    return;
   }
-  
-  auto total = v8::Number::New(isolate, a);
 
-  args.GetReturnValue().Set(total);
+  double infoValue = info[0]->NumberValue();
+  if (numberOfCalls + infoValue > maxValue) {
+    Nan::ThrowError("Counter went through the roof!");
+    return;
+  }
+
+  numberOfCalls += infoValue;
+
+  auto currentNumberOfCalls =
+    Nan::New<v8::Number>(numberOfCalls);
+
+  info.GetReturnValue().Set(currentNumberOfCalls);
 }
 
-void Initialize(v8::Local<v8::Object> exports){
-  NODE_SET_METHOD(exports, "sum", Sum);
+NAN_MODULE_INIT(Initialize) {
+  NAN_EXPORT(target, WhoAmI);
+  NAN_EXPORT(target, Increment);
 }
 
-NAN_MODULE_WORKER_ENABLED (addon, Initialize)
+NODE_MODULE(addon, Initialize)
