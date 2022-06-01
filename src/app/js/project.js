e0a98d83c69993b4b5c18bc8c40ada39037067f4
FernandoDC-MX <fernando@dcircuits.mx>
2022-06-01 08:07:59 -0500

    Diseño de contenedores

--- src/app/js/project.js
+++ src/app/js/project.js
@@ -82,7 +82,7 @@ function displayChannels(canales){
 
 function displayGraphs(canales){
     const _canales = new Map(Object.entries(canales));
-    const _zone = document.querySelector('.graphs');
+    const _zone = document.querySelector('.zone');
 
     _canales.forEach(element =>{
         if(element._id_muscle){
@@ -92,12 +92,8 @@ function displayGraphs(canales){
             _div.classList.add('main-graph-container', 'row');
             _div.setAttribute('data-device', element.id)
 
-            _div.appendChild(createMenu())
-
             var _graph = document.createElement('div');
             _graph.classList.add('col-8','col-md-8', 'col-sm-12', 'pl-0', 'text-center');
-
-            
 
             var _graph_div = document.createElement('div');
             _graph.appendChild(_graph_div)
@@ -115,28 +111,185 @@ function displayGraphs(canales){
         }
     })
 
-    listenerTests();
 }
 
-function listenerTests(){
-    var _playButtons = document.querySelectorAll('.play-icon');
-
-    for(let i = 0; i < _playButtons.length; i++){
-        _playButtons[i].addEventListener('click', function(){
-            let id = this.parentNode.parentNode.getAttribute('data-device');
-            startDataGraph(id, this.parentNode.parentNode);
-        })
-    }
-}
+playBtn.addEventListener('click', function(){
+    startDataGraph();
+});
 
 // 
-function startDataGraph(id, _div){
-    var color = channels.get(id)._hex;
+function startDataGraph(){
+    // Gets all the main charts.
+    var _mainCharts = document.querySelectorAll('.main-graph-container');
 
-    var _mainDiv = _div;
+    createCharts(_mainCharts)
 
-    var _dataDiv = _mainDiv.querySelector('.col-8')
-    const _canvas = document.createElement('canvas')
+
+    // var color = channels.get(id)._hex;
+
+    // var _mainDiv = _div;
+
+    // var _dataDiv = _mainDiv.querySelector('.col-8')
+    // const _canvas = document.createElement('canvas')
+    // const ctx = _canvas.getContext('2d');
+
+    // // Device chart
+    // var chart = new Chart(ctx, {
+    //     type: "line",
+    //     data: {
+    //         labels: [],
+    //         datasets: [{
+    //             backgroundColor: "#" + color,
+    //             borderColor: "#" + color,
+    //             label: 'Dispositivo',
+    //             data: []
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             y: {
+    //                 beginAtZero: true
+    //             }
+    //         },
+    //         plugins: {
+    //             zoom: {
+    //                 zoom: {
+    //                   wheel: {
+    //                     enabled: true,
+    //                     speed: 0.1,
+    //                   },
+    //                   drag: {
+    //                     enabled: true
+    //                   },
+    //                   pinch: {
+    //                     enabled: true
+    //                   },
+    //                   mode: 'xy',
+    //                 },
+    //                 limits: {
+    //                     y: {min: 1, max: 100},}
+    //             }
+    //         },
+    //         transitions: {
+    //             zoom: {
+    //               animation: {
+    //                 duration: 1000,
+    //                 easing: 'easeOutCubic'
+    //               }
+    //             }
+    //         },
+    //         responsive: true,
+    //         maintainAspectRatio: false
+    //     }
+    // });
+
+    // // Chart.register(zoomPlugin)
+    // _dataDiv.innerHTML = '';
+    // _dataDiv.appendChild(_canvas)
+
+    // var _subgraphs = _mainDiv.querySelector('.col-4');
+    // _subgraphs.innerHTML = ''
+
+    // var _div = document.createElement('div');
+    // _div.classList.add('subgraph')
+
+
+    // const _accelerometerCanvas = document.createElement('canvas')
+    // const _accelerometerCtx = _accelerometerCanvas.getContext('2d');
+
+    // // Device chart
+    // var _accelerometerChart = new Chart(_accelerometerCtx, {
+    //     type: "line",
+    //     data: {
+    //         labels: [],
+    //         datasets: [{
+    //             backgroundColor: "#" + color,
+    //             borderColor: "#" + color,
+    //             label: 'Acelerómetro',
+    //             data: []
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             y: {
+    //                 beginAtZero: true
+    //             }
+    //         },
+    //         responsive: true,
+    //         maintainAspectRatio: false
+    //     }
+    // });
+
+    // _div.appendChild(_accelerometerCanvas)
+
+    // _subgraphs.appendChild(_div)
+
+    // var _div = document.createElement('div');
+    // _div.classList.add('subgraph')
+
+    // const _gyroCanvas = document.createElement('canvas')
+    // const _gyroCtx = _gyroCanvas.getContext('2d');
+
+    // const _gyroChart = new Chart(_gyroCtx, {
+    //     type: "line",
+    //     data: {
+    //         labels: [],
+    //         datasets: [{
+    //             backgroundColor: "#" + color,
+    //             borderColor: "#" + color,
+    //             label: 'Giroscopio',
+    //             data: []
+    //         }]
+    //     },
+    //     options: {
+    //         scales: {
+    //             y: {
+    //                 beginAtZero: true
+    //             }
+    //         },
+    //         responsive: true,
+    //         maintainAspectRatio: false
+    //     }
+    // });
+
+
+    // _div.appendChild(_gyroCanvas)
+
+    // _subgraphs.appendChild(_div)
+
+    // Chart.register(zoomPlugin)
+
+
+    // demo(chart, _accelerometerChart, _gyroChart);
+}
+
+
+function createCharts(_divs){
+    // We will iterate each chart zone by each device linked within the project.
+    for(let i = 0; i < _divs.length; i++){
+        var _mainContainer = _divs[i];
+
+        var _device = channels.get(_mainContainer.getAttribute('data-device'))
+
+        var _mainChart = _mainContainer.querySelector('.col-8');
+        _mainChart.innerHTML = ''
+
+        var _secondaryCharts = _mainContainer.querySelector('.col-4');
+        _secondaryCharts.innerHTML = '';
+
+        _mainChart.appendChild(drawMainChart(_device._hex))
+
+        _canvas = document.createElement("canvas");
+        _secondaryCharts.appendChild(_canvas)
+
+        _canvas = document.createElement("canvas");
+        _secondaryCharts.appendChild(_canvas)
+    }
+
+}
+
+function drawMainChart(color){
+    const _canvas = document.createElement("canvas");
     const ctx = _canvas.getContext('2d');
 
     // Device chart
@@ -189,84 +342,7 @@ function startDataGraph(id, _div){
         }
     });
 
-    // Chart.register(zoomPlugin)
-    _dataDiv.innerHTML = '';
-    _dataDiv.appendChild(_canvas)
-
-    var _subgraphs = _mainDiv.querySelector('.col-4');
-    _subgraphs.innerHTML = ''
-
-    var _div = document.createElement('div');
-    _div.classList.add('subgraph')
-
-
-    const _accelerometerCanvas = document.createElement('canvas')
-    const _accelerometerCtx = _accelerometerCanvas.getContext('2d');
-
-    // Device chart
-    var _accelerometerChart = new Chart(_accelerometerCtx, {
-        type: "line",
-        data: {
-            labels: [],
-            datasets: [{
-                backgroundColor: "#" + color,
-                borderColor: "#" + color,
-                label: 'Acelerómetro',
-                data: []
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
-
-    _div.appendChild(_accelerometerCanvas)
-
-    _subgraphs.appendChild(_div)
-
-    var _div = document.createElement('div');
-    _div.classList.add('subgraph')
-
-    const _gyroCanvas = document.createElement('canvas')
-    const _gyroCtx = _gyroCanvas.getContext('2d');
-
-    const _gyroChart = new Chart(_gyroCtx, {
-        type: "line",
-        data: {
-            labels: [],
-            datasets: [{
-                backgroundColor: "#" + color,
-                borderColor: "#" + color,
-                label: 'Giroscopio',
-                data: []
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        }
-    });
-
-
-    _div.appendChild(_gyroCanvas)
-
-    _subgraphs.appendChild(_div)
-
-    Chart.register(zoomPlugin)
-
-
-    demo(chart, _accelerometerChart, _gyroChart);
+    return _canvas;
 }
 
 function sleep(ms) {
@@ -418,40 +494,6 @@ var lineChart = new Chart(_graphic, {
 
 
     return _canvas;
-}
-
-const createMenu = () =>{
-    var _menu = document.createElement('div');
-    _menu.classList.add('menu');
-
-  
-    const _play = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-play-circle play-icon mx-2" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
-                    </svg>`;
-
-    const _pause = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-pause-circle pause-icon mx-2" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M5 6.25a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5zm3.5 0a1.25 1.25 0 1 1 2.5 0v3.5a1.25 1.25 0 1 1-2.5 0v-3.5z"/>
-                    </svg>`;
-    
-    const _config = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-gear setting-icon mx-2" viewBox="0 0 16 16">
-                        <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
-                        <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
-                    </svg>`;
-    
-    const _download = `<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-download download-icon mx-2" viewBox="0 0 16 16">
-                            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-                            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-                        </svg>`;
-
-    _menu.innerHTML+= _play;
-    _menu.innerHTML+= _pause;
-    _menu.innerHTML+= _config;
-    _menu.innerHTML+= _download;
-
-
-    return _menu
 }
 
 function setId(){
