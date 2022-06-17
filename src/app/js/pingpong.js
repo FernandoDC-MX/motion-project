aafc0c671317e3e207275522b48e8882cfbd3b7d
FernandoDC-MX <fernando@dcircuits.mx>
2022-06-17 09:55:41 -0500

    Diseño

--- src/app/js/pingpong.js
+++ src/app/js/pingpong.js
@@ -23,51 +23,41 @@ document.addEventListener('keydown', (e) => {
 	if (e.key == 'Enter') {
 		gameState = gameState == 'start' ? 'play' : 'start';
 		if (gameState == 'play') {
-		document.querySelector('#goal-notification').style.animation = '';
-		message.innerHTML = '';
-		message.style.left = 42 + 'vw';
-		requestAnimationFrame(() => {
-			dx = Math.floor(Math.random() * 4) + 3;
-			dy = Math.floor(Math.random() * 4) + 3;
-			dxd = Math.floor(Math.random() * 2);
-			dyd = Math.floor(Math.random() * 2);
-			moveBall(dx, dy, dxd, dyd);
-		});
+			document.querySelector('#goal-notification').style.animation = '';
+
+			message.innerHTML = '';
+			message.style.left = 42 + 'vw';
+
+			requestAnimationFrame(() => {
+				dx = Math.floor(Math.random() * 4) + 3;
+				dy = Math.floor(Math.random() * 4) + 3;
+				dxd = Math.floor(Math.random() * 2);
+				dyd = Math.floor(Math.random() * 2);
+				moveBall(dx, dy, dxd, dyd);
+			});
 		}
 	}
-	if (gameState == 'play') {
-		if (e.key == 'w') {
-		paddle_1.style.top =
-			Math.max(
-			board_coord.top,
-			paddle_1_coord.top - window.innerHeight * 0.06
-			) + 'px';
-		paddle_1_coord = paddle_1.getBoundingClientRect();
-		}
-		if (e.key == 's') {
-		paddle_1.style.top =
-			Math.min(
-			board_coord.bottom - paddle_common.height,
-			paddle_1_coord.top + window.innerHeight * 0.06
-			) + 'px';
-		paddle_1_coord = paddle_1.getBoundingClientRect();
-		}
 
-		if (e.key == 'ArrowUp') {
-		paddle_2.style.top =
-			Math.max(
-			board_coord.top,
-			paddle_2_coord.top - window.innerHeight * 0.1
-			) + 'px';
-		paddle_2_coord = paddle_2.getBoundingClientRect();
+	if (gameState == 'play') {
+		// Up move (Left player)
+		if (e.key == 'w') {
+			paddle_1.style.top = Math.max(board_coord.top, paddle_1_coord.top - window.innerHeight * 0.06) + 'px';
+			paddle_1_coord = paddle_1.getBoundingClientRect();
 		}
+		// Down move (left player)
+		if (e.key == 's') {
+			paddle_1.style.top = Math.min( board_coord.bottom - paddle_common.height, paddle_1_coord.top + window.innerHeight * 0.06) + 'px';
+			paddle_1_coord = paddle_1.getBoundingClientRect();
+		}
+		// Up move (Right player)
+		if (e.key == 'ArrowUp') {
+			paddle_2.style.top = Math.max (board_coord.top,paddle_2_coord.top - window.innerHeight * 0.1) + 'px';
+			paddle_2_coord = paddle_2.getBoundingClientRect();
+		}
+		// Down move (Right player)
 		if (e.key == 'ArrowDown') {
-		paddle_2.style.top =
-			Math.min(
-			board_coord.bottom - paddle_common.height,
-			paddle_2_coord.top + window.innerHeight * 0.1
-			) + 'px';
-		paddle_2_coord = paddle_2.getBoundingClientRect();
+			paddle_2.style.top = Math.min( board_coord.bottom - paddle_common.height, paddle_2_coord.top + window.innerHeight * 0.1) + 'px';
+			paddle_2_coord = paddle_2.getBoundingClientRect();
 		}
 	}
 });
