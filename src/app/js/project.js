a96f7d35e055987f6b78eb32bbbd936aa8d156f3
FernandoDC-MX <fernando@dcircuits.mx>
2022-06-22 10:30:50 -0500

    Despliegue de archivos funcionando

--- src/app/js/project.js
+++ src/app/js/project.js
@@ -88,6 +88,7 @@ let _devices = [{'hola': '1'}];
 var _chartsMap = new Map()
 let arrChilds = new Map()
 let _master = new MasterDevice()
+let _filesData = null;
 
 // Set the window's title.
 ipc.on('enviar-nombre', (e, title) =>{ 
@@ -1404,40 +1405,12 @@ fileExplorer.addEventListener('hidden.bs.modal', function(){
 })
 
 fileExplorer.addEventListener('shown.bs.modal', function(){
-    const response =  readDataFolder(_path + '\\Data');
+    _filesData =  readDataFolder(_path + '\\Data');
 
-    if(response.length){
-        let tbody = document.querySelector('#fileExplorer table tbody')
-        tbody.innerHTML = '';
+    if(_filesData.length){
+       displayListFiles() 
 
-        response.forEach(_file =>{
-            var _tr = document.createElement('tr');
-
-            var _td = document.createElement('td');
-            _td.innerText = _file.name
-
-            _tr.appendChild(_td)
-
-            _td = document.createElement('td');
-            _td.innerText = _file.updated_time;
-
-            _tr.appendChild(_td)
-
-            _td = document.createElement('td');
-            _td.innerText = 'Archivo ' + _file.type;
-
-            _tr.appendChild(_td)
-
-            _td = document.createElement('td');
-            _td.innerText = _file.size + ' KB'
-
-            _tr.appendChild(_td)
-
-            tbody.appendChild(_tr)
-       })
-
-    
-       document.querySelector('.info .elements').innerHTML = response.length + ' elementos';
+       document.querySelector('.info .elements').innerHTML = _filesData.length + ' elementos';
        document.querySelector('#fileExplorer .table-responsive').classList.remove('d-none')
     }else{
         alert('Error')
@@ -1446,25 +1419,96 @@ fileExplorer.addEventListener('shown.bs.modal', function(){
     document.querySelector('.waiting').classList.add('d-none');
 })
 
-document.querySelectorAll('#fileExplorer .files .col-2 div').forEach(element => {
-    console.log(element)
+document.querySelectorAll('#fileExplorer .files .col-2 .row').forEach(element => {
+    element.addEventListener('click', function(){
+        if(document.querySelector('.selected-option'))
+            document.querySelector('.selected-option').classList.remove('selected-option')
+        
+        this.classList.add('selected-option')
+
+        const val = this.getAttribute('data-value')
+
+    });
 })
 
+document.querySelectorAll('#fileExplorer .info svg').forEach(element => {
+    element.addEventListener('click', function(){
+        if(document.querySelector('.selected-svg'))
+            document.querySelector('.selected-svg').classList.remove('selected-svg')
+        
+        this.classList.add('selected-svg')
 
-listFiles.addEventListener('click', function(){
-    this.nextElementSibling.classList.remove('selected-svg')
-    this.classList.add('selected-svg')
+        const val = parseInt(this.getAttribute('data-value'))
 
+        switch(val){
+            case 0: zone = document.querySelector('.table-responsive').classList.remove('d-none')
+                    document.querySelector('.images-responsive').classList.add('d-none')
+                    displayListFiles()
+                break;
+            case 1: zone = document.querySelector('.images-responsive').classList.remove('d-none')
+                    document.querySelector('.table-responsive').classList.add('d-none')
+                    displayImageFiles()
+                break;
+        }
+    });
 })
 
-imgFiles.addEventListener('click', function(){
-    this.previousElementSibling.classList.remove('selected-svg')
-    this.classList.add('selected-svg')
-})
+function displayListFiles(){
+    let tbody = document.querySelector('#fileExplorer table tbody')
+    tbody.innerHTML = '';
 
-function readDataFolder(){
+    _filesData.forEach(_file =>{
+        var _tr = document.createElement('tr');
 
-};
+        var _td = document.createElement('td');
+        _td.innerText = _file.name
+
+        _tr.appendChild(_td)
+
+        _td = document.createElement('td');
+        _td.innerText = _file.updated_time;
+
+        _tr.appendChild(_td)
+
+        _td = document.createElement('td');
+        _td.innerText = 'Archivo ' + _file.type;
+
+        _tr.appendChild(_td)
+
+        _td = document.createElement('td');
+        _td.innerText = _file.size + ' KB'
+
+        _tr.appendChild(_td)
+
+        tbody.appendChild(_tr)
+    })
+}
+
+function displayImageFiles(){
+    let body = document.querySelector('#fileExplorer .images-responsive')
+    body.innerHTML = ''
+
+    const svg = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-filetype-json" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M14 4.5V11h-1V4.5h-2A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v9H2V2a2 2 0 0 1 2-2h5.5L14 4.5ZM4.151 15.29a1.176 1.176 0 0 1-.111-.449h.764a.578.578 0 0 0 .255.384c.07.049.154.087.25.114.095.028.201.041.319.041.164 0 .301-.023.413-.07a.559.559 0 0 0 .255-.193.507.507 0 0 0 .084-.29.387.387 0 0 0-.152-.326c-.101-.08-.256-.144-.463-.193l-.618-.143a1.72 1.72 0 0 1-.539-.214 1.001 1.001 0 0 1-.352-.367 1.068 1.068 0 0 1-.123-.524c0-.244.064-.457.19-.639.128-.181.304-.322.528-.422.225-.1.484-.149.777-.149.304 0 .564.05.779.152.217.102.384.239.5.41.12.17.186.359.2.566h-.75a.56.56 0 0 0-.12-.258.624.624 0 0 0-.246-.181.923.923 0 0 0-.37-.068c-.216 0-.387.05-.512.152a.472.472 0 0 0-.185.384c0 .121.048.22.144.3a.97.97 0 0 0 .404.175l.621.143c.217.05.406.12.566.211a1 1 0 0 1 .375.358c.09.148.135.335.135.56 0 .247-.063.466-.188.656a1.216 1.216 0 0 1-.539.439c-.234.105-.52.158-.858.158-.254 0-.476-.03-.665-.09a1.404 1.404 0 0 1-.478-.252 1.13 1.13 0 0 1-.29-.375Zm-3.104-.033a1.32 1.32 0 0 1-.082-.466h.764a.576.576 0 0 0 .074.27.499.499 0 0 0 .454.246c.19 0 .33-.055.422-.164.091-.11.137-.265.137-.466v-2.745h.791v2.725c0 .44-.119.774-.357 1.005-.237.23-.565.345-.985.345a1.59 1.59 0 0 1-.568-.094 1.145 1.145 0 0 1-.407-.266 1.14 1.14 0 0 1-.243-.39Zm9.091-1.585v.522c0 .256-.039.47-.117.641a.862.862 0 0 1-.322.387.877.877 0 0 1-.47.126.883.883 0 0 1-.47-.126.87.87 0 0 1-.32-.387 1.55 1.55 0 0 1-.117-.641v-.522c0-.258.039-.471.117-.641a.87.87 0 0 1 .32-.387.868.868 0 0 1 .47-.129c.177 0 .333.043.47.129a.862.862 0 0 1 .322.387c.078.17.117.383.117.641Zm.803.519v-.513c0-.377-.069-.701-.205-.973a1.46 1.46 0 0 0-.59-.63c-.253-.146-.559-.22-.916-.22-.356 0-.662.074-.92.22a1.441 1.441 0 0 0-.589.628c-.137.271-.205.596-.205.975v.513c0 .375.068.699.205.973.137.271.333.48.589.626.258.145.564.217.92.217.357 0 .663-.072.917-.217.256-.146.452-.355.589-.626.136-.274.205-.598.205-.973Zm1.29-.935v2.675h-.746v-3.999h.662l1.752 2.66h.032v-2.66h.75v4h-.656l-1.761-2.676h-.032Z"/>
+                </svg>`
+
+    _filesData.forEach(file => {
+        let div = document.createElement('div')
+        div.classList.add('card-file', 'd-flex-column', 'align-items-center', 'justify-content-center')
+        div.setAttribute('title', file.name)
+
+        div.innerHTML+= svg
+
+        let p = document.createElement('p');
+        p.classList.add('text-center')
+        p.innerHTML = file.name;
+
+        div.appendChild(p)
+
+        body.appendChild(div)
+    });
+
+}
 
 linkModal.addEventListener('hidden.bs.modal', function(){
     document.querySelector('#_deviceMac').value = '';
@@ -1487,3 +1531,4 @@ devicesBtn.addEventListener('click', () =>{
     tmp.nextElementSibling.classList.remove('d-none')
     tmp.classList.contains('d-none') ? tmp.classList.remove('d-none') : tmp.classList.add('d-none')
 })
+
