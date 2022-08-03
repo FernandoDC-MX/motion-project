07883b578eb4ea9911f2b119a38beea592d08844
ferbargar99@hotmail.com <ferbargar99@hotmail.com>
2022-08-03 16:00:11 -0500

    Validación de procesos

--- src/app/js/spawn_process.js
+++ src/app/js/spawn_process.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 
 let buffer = []
 let nMaxIterations = 20000;
-let nRefresh = 100;
+let nRefresh = 50;
 let iterator = 0;
 
 process.on('message', (args) => {
